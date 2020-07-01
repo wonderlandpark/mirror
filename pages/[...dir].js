533ce0c0759b1930bdca0ca556e8f0e3382164c4
원더 <wonderbotsupport@naver.com>
2020-07-01 12:25:13 +0900

    First Commit

--- pages/[...dir].js
+++ pages/[...dir].js
@@ -1,24 +1,26 @@
 import Head from 'next/head'
 import { Table, Icon } from 'semantic-ui-react'
 
-function Home({ dir }) {
+function Home({ path, dir }) {
   return (
     <div className="ui top container">
-    <Head>
-      <title>WonderMirror</title>
-      <meta property="og:description" content="Wonder is GOD."/>
-      <meta property="og:title" content="WonderMirror"/>
-    </Head>
       <h1 className="ui header">Wonder Mirror</h1>
 
     <Table>
     <Table.Header>
       <Table.Row>
-        <Table.HeaderCell colSpan='3'>/</Table.HeaderCell>
+        <Table.HeaderCell colSpan='3'>/{path}</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
 
       <Table.Body>
+        <Table.Row>
+              <Table.Cell collapsing>
+                <Icon name='folder open outline' />
+                <a href={"/" + path.split('/').slice(1).join('/')}>..</a>
+              </Table.Cell>
+              
+            </Table.Row>
         {
           dir.map(r=> 
             <>
@@ -28,7 +30,7 @@ function Home({ dir }) {
                 r.isDir ? ( 
                   <>
                   <Icon name='folder outline' />
-                  <a href={r.name}>{r.name}</a>
+                  <a href={ path + '/' + r.name}>{r.name}</a>
                   </>
                 ) : (
                   <>
@@ -45,16 +47,25 @@ function Home({ dir }) {
         }
       </Table.Body>
     </Table>
+      {
+        dir.map(r=> JSON.stringify(r))
+      }
     </div>
   )
 }
 
-Home.getInitialProps = () => {
+Home.getInitialProps = (ctx) => {
   const fs = require('fs')
   const path = require('path')
 
-  const dir = fs.readdirSync('./files')
 
-  return { dir: dir.map(el=> { return { isDir: fs.lstatSync(path.join('./files', el)).isDirectory(), name: el } } )}
+  const paths = ctx.query.dir
+  const pathText = paths.join('/')
+  paths.unshift('./files')
+
+  console.log()
+  const dir = fs.readdirSync(path.join(...paths))
+
+  return { dir: dir.map(el=> { return { isDir: fs.lstatSync(path.join(...paths, el)).isDirectory(), name: el } } ).sort((x,y) => (x.isDir===y.isDir) ? 0 : x.isDir? -1 : 1), path: pathText }
 }
 export default Home
---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import 'semantic-ui-css/semantic.css'
+import './App.css'
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} key="app"/>
+  }
